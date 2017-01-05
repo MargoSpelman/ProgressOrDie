3a5f647d56d357a6a06b079321d3e6b33bc6291a
Isaiah Mann <contact@isaiahmann.com>
2017-01-05 15:27:13 -0500

    Pushing basic parser module

---
+++ Assets/Scripts/Modules/Parser/IParserModule.cs
@@ -0,0 +1,11 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Desc: Parses json into C# Classes
+ */
+
+public interface IParserModule {
+
+	T ParseFromResources<T> (string pathInResources);
+	T Parse<T> (string json);
+
+}
---
+++ Assets/Scripts/Modules/Parser/ParserModule.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Desc:
+ */
+
+using UnityEngine;
+
+public class ParserModule : Module, IParserModule {
+
+	public T ParseFromResources<T> (string path) {
+		string json = getTextAssetInResources(path).text;
+		return Parse<T>(json);
+	}
+
+	public T Parse<T> (string json) {
+		return JsonUtility.FromJson<T>(json);
+	}
+
+	TextAsset getTextAssetInResources (string path) {
+		return Resources.Load<TextAsset>(path);
+	}
+
+}
