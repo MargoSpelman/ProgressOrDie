35c8a00e9be347a733a6b2df41bc03a830d4b291
Isaiah Mann <contact@isaiahmann.com>
2017-01-06 15:17:01 -0500

    Adding game end module with interface for legends

---
+++ Assets/Scripts/Modules/GameEnd/IGameEndModule.cs
@@ -0,0 +1,10 @@
+﻿/*
+ * Authors: Isaiah Mann
+ * Description: An interface to end of game logic
+ */
+
+public interface IGameEndModule {
+
+	void ProcessGameEnd(IPlayerCharacter player, GameEndType endType);
+
+}
---
+++ Assets/Scripts/Modules/GameEnd/IPlayerLegend.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: A title the player can earn / be penalized with at the end of the round
+ */
+
+public interface IPlayerLegend {
+
+	string GetId();
+
+	// Current vals are Flee or Win (Death does not apply)
+	GameEndType GetCause();
+
+	// How many times you flee or win to earn this title
+	int StreakCountToEarn();
+
+	int GetStrengthModifier();
+	int GetBulkModifier();
+	int GetSoulModifier();
+
+}
