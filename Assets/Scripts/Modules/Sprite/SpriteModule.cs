9eeef925efd38799fc03af29b0eaec90e430ab96
Isaiah Mann <contact@isaiahmann.com>
2017-01-08 13:34:25 -0500

    Creating empty sprite loading class

---
+++ Assets/Scripts/Modules/Sprite/SpriteModule.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: Loads sprites for the game from resources
+ * Usage: [no notes]
+ */
+
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SpriteModule : Module 
+{	
+	Dictionary<string, Sprite> tileSpriteLookup = new Dictionary<string, Sprite>();
+
+	public Sprite GetTile(TileType type)
+	{
+		throw new System.NotImplementedException();
+	}
+
+}
