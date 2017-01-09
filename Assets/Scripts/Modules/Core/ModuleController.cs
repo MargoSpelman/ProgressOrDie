0132c2f1149139744c38aef689e29f89827ec663
Isaiah Mann <contact@isaiahmann.com>
2017-01-09 10:02:41 -0500

    Parsing ability JSON

--- Assets/Scripts/Modules/Core/ModuleController.cs
+++ Assets/Scripts/Modules/Core/ModuleController.cs
@@ -54,16 +54,20 @@ public class ModuleController : SingletonController<ModuleController> {
 	protected override void SetReferences ()
 	{
 		base.SetReferences ();
-		EnemyData enemyData = parser.ParseJSONFromResources<EnemyData>("Enemies");
 		TileData tileData = parser.ParseJSONFromResources<TileData>("Tiles");
 		string[,] tiles = parser.ParseCSVFromResources("Example/Tiles");
 		map.Init(tiles, tileData.Tiles, sprites);
+
+		EnemyData enemyData = parser.ParseJSONFromResources<EnemyData>("Enemies");
 		string[,] units = parser.ParseCSVFromResources("Example/Units");
 		unit.Init(map, sprites, units, enemyData, turn, movement, combat, stats, abilities);
 		cam.StartFollowing(unit.GetMainPlayer());
 		ui.Init(turn, unit);
 		movement.Init(turn);
 		combat.Init(unit, map, abilities, stats, gameEnd);
+
+		AbilityData abilityData = parser.ParseJSONFromResources<AbilityData>("Abilities");
+		abilities.Init(abilityData);
 	}
 		
 }
