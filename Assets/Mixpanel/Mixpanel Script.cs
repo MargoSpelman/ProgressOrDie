fdd0e277c3215944e2164393f2b2d21edc83f256
Riva <lam23r@mtholyoke.edu>
2017-01-09 13:56:16 -0500

    Token

---
+++ Assets/Mixpanel/Mixpanel Script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MixpanelScript : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () { Mixpanel.Token= "1b6f1caa2d59c490c189621be1ea58e3";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
